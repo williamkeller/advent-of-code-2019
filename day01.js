082f9ac55ece6e7bc26c3fe63ee5cfb79cdfffdf
William Keller <william@arda.net>
2019-12-02 23:38:45 -0500

    Using babel for better language features

--- day01.js
+++ day01.js
@@ -1,33 +1,31 @@
 const input = require('./input')
 
-module.exports = {
-  fuel_for_mass: function(mass) {
-    return Math.floor(mass / 3.0) - 2
-  },
+export function fuel_for_mass(mass) {
+  return Math.floor(mass / 3.0) - 2
+}
 
-  recursive_fuel_for_mass: function(mass) {
-    let fuel = Math.floor(mass / 3.0) - 2
-    if(fuel > 0)
-      return fuel + module.exports.recursive_fuel_for_mass(fuel)
-    else
-      return 0
-  },
+export function recursive_fuel_for_mass(mass) {
+  let fuel = Math.floor(mass / 3.0) - 2
+  if(fuel > 0)
+    return fuel + recursive_fuel_for_mass(fuel)
+  else
+    return 0
+}
 
-  puzzle: function(calculator, callback) {
-    let total_fuel = 0
+export function puzzle(calculator, callback) {
+  let total_fuel = 0
 
-    input.readByLine('data/day01_input.txt', (mass) => {
-      if(mass !== null) {
-        total_fuel += calculator(mass)
-      }
-      else {
-        callback(total_fuel)
-      }
-    })
-  }
+  input.readByLine('data/day01_input.txt', (mass) => {
+    if(mass !== null) {
+      total_fuel += calculator(mass)
+    }
+    else {
+      callback(total_fuel)
+    }
+  })
 }
 
 if(process.argv[1] === __filename) {
-  module.exports.puzzle(module.exports.fuel_for_mass, (value) => { console.log(`puzzle1: ${value}`) })
-  module.exports.puzzle(module.exports.recursive_fuel_for_mass, (value) => { console.log(`puzzle2: ${value}`) })
+  puzzle(fuel_for_mass, (value) => { console.log(`puzzle1: ${value}`) })
+  puzzle(recursive_fuel_for_mass, (value) => { console.log(`puzzle2: ${value}`) })
 }
--- day02.js
+++ day02.js
@@ -1,55 +1,53 @@
 const fs = require('fs')
 
-module.exports = {
+export function interpreter(data, noun, verb) {
+  let ip = 0
+  let running = true
 
-  interpreter: function(data, noun, verb) {
-    let ip = 0
-    let running = true
+  data[1] = noun
+  data[2] = verb
 
-    data[1] = noun
-    data[2] = verb
+  while(running) {
+    let opcode = data[ip]
 
-    while(running) {
-      opcode = data[ip]
-
-      if(opcode === 99) {
-        running = false
-        break;
-      }
-
-      let addr1 = data[ip + 1]
-      let addr2 = data[ip + 2]
-      let addr3 = data[ip + 3]
-      let arg1 = data[addr1]
-      let arg2 = data[addr2]
-
-      switch(opcode) {
-        case 1:
-          data[addr3] = arg1 + arg2
-          break
-        case 2:
-          data[addr3] = arg1 * arg2
-          break
-      }
-
-      if(running == false)
-        break
-
-      ip += 4
+    if(opcode === 99) {
+      running = false
+      break;
     }
-    return data[0]
+
+    let addr1 = data[ip + 1]
+    let addr2 = data[ip + 2]
+    let addr3 = data[ip + 3]
+    let arg1 = data[addr1]
+    let arg2 = data[addr2]
+
+    switch(opcode) {
+      case 1:
+        data[addr3] = arg1 + arg2
+        break
+      case 2:
+        data[addr3] = arg1 * arg2
+        break
+    }
+
+    if(running == false)
+      break
+
+    ip += 4
   }
+  return data[0]
 }
+
 
 if(process.argv[1] === __filename) {
   const data = JSON.parse(fs.readFileSync('data/day02_input.json'))
 
-  let result = module.exports.interpreter([...data], 12, 2)
+  let result = interpreter([...data], 12, 2)
   console.log(result)
 
   for(let i = 0; i < 100; i++) {
     for(let j = 0; j < 100; j++) {
-      let result = module.exports.interpreter([...data], i, j)
+      let result = interpreter([...data], i, j)
       if(result === 19690720) {
         console.log(`noun = ${i}, verb = ${j}`)
       }
