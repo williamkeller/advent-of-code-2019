85f951fa99df67f864b8ca18db351d659a7b58f2
William Keller <william@arda.net>
2019-12-04 00:43:26 -0500

    Refactor, less gross

--- day04.js
+++ day04.js
@@ -1,15 +1,12 @@
 
-
-
-export function findPasswordRange(start, finish) {
+export function findPasswordRange(start, finish, validFunc) {
   let count = 0
 
   start = parseInt(start)
   finish = parseInt(finish)
 
   for(let i = start; i <= finish; i++) {
-    if(isValidPassword(i.toString())) {
-      console.log(i.toString())
+    if(validFunc(i.toString())) {
       count += 1
     }
   }
@@ -17,7 +14,22 @@ export function findPasswordRange(start, finish) {
   return count
 }
 
-export function isValidPassword(pwd) {
+export function isValidPassword1(pwd) {
+  let adjCount = 0
+
+  for(let i = 0; i < 5; i++) {
+    if(parseInt(pwd[i]) > parseInt(pwd[i + 1])) {
+      return false
+    }
+    if(parseInt(pwd[i]) == parseInt(pwd[i + 1])) {
+      adjCount += 1
+    }
+  }
+
+  return adjCount > 0
+}
+
+export function isValidPassword2(pwd) {
   let adjCount = 0
   let adjCounts = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
@@ -31,8 +43,6 @@ export function isValidPassword(pwd) {
     }
   }
 
-  // console.log(pwd)
-  // console.log(adjCounts)
   for(let a of adjCounts) {
     if(a == 2)
       return true
@@ -44,15 +54,11 @@ export function isValidPassword(pwd) {
 
 if(process.argv[1] === __filename) {
 
-  // console.log(isValidPassword('111111'))
-  // console.log(isValidPassword('223450'))
-  // console.log(isValidPassword('123789'))
+  let r
+  r = findPasswordRange('172930', '683082', isValidPassword1)
+  console.log(`puzzle 1: ${r}`)
 
-  // console.log(isValidPassword('112233'))
-  // console.log(isValidPassword('123444'))
-  // console.log(isValidPassword('111122'))
-
-  let r = findPasswordRange('172930', '683082')
-  console.log(r)
+  r = findPasswordRange('172930', '683082', isValidPassword2)
+  console.log(`puzzle 2: ${r}`)
 }
 
